e1728d66a33aef8b58e6da49e8f299e78bd1a181
Fernando Baladei <f.baladei@gmail.com>
2023-02-15 06:24:06 -0300

    trying

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,11 @@ import './styles/about.css'
 export const About = () => {
   return (
     <div className='about'>
-        <img src={Avatar} alt="Fernando Baladei" />        
-        <p><h1>Web Developer</h1><br></br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, optio earum iure rerum officiis aspernatur. Iure aspernatur nulla ex, explicabo illo iusto in suscipit quae eaque totam necessitatibus reiciendis itaque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta illum neque magni ad eaque quas delectus laudantium eveniet quos. Libero facilis vel incidunt amet aspernatur? Harum fugit eligendi velit officia. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, saepe quia velit fugit blanditiis necessitatibus tenetur assumenda nisi architecto impedit rem, enim vitae, quam sit. Rem maxime perspiciatis eius unde.</p>
+        <img src={Avatar} alt="Fernando Baladei" />
+        <div className="descrip">
+        <h1>Web Developer</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, optio earum iure rerum officiis aspernatur. Iure aspernatur nulla ex, explicabo illo iusto in suscipit quae eaque totam necessitatibus reiciendis itaque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta illum neque magni ad eaque quas delectus laudantium eveniet quos. Libero facilis vel incidunt amet aspernatur? Harum fugit eligendi velit officia. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, saepe quia velit fugit blanditiis necessitatibus tenetur assumenda nisi architecto impedit rem, enim vitae, quam sit. Rem maxime perspiciatis eius unde.</p>
+        </div>        
     </div>
   )
 }
